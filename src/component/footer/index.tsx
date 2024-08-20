8b5f896263b0d06dfabe70ba356bbc099a4dccaa
Molly <9390548+MollyLove@user.noreply.gitee.com>
2024-08-20 16:16:12 +0800

    15

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -5,7 +5,7 @@ export default function footerContainer() {
   return (
     <footer className="bottom">
       <nav className="left">
-        <a className="textStyle">About Google</a>
+        <a className="textStyle">关于 Google</a>
         <a className="textStyle">广告</a>
         <a className="textStyle">商务</a>
         <a className="textStyle">Google 搜索运动方式</a>
