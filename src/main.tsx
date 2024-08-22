08f64bd2a5c1eea8db9ced185c919e112baaef6b
Molly <9390548+MollyLove@user.noreply.gitee.com>
2024-08-22 11:58:48 +0800

    15

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import App from './App';
 import ReactDOM, { Root } from 'react-dom/client'; // 引入 createRoot 和 Root 类型
 import store from './store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const rootElement: HTMLDivElement | null = document.getElementById(
   'root'
@@ -10,6 +18,6 @@ const rootElement: HTMLDivElement | null = document.getElementById(
 const root: Root = ReactDOM.createRoot(rootElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
